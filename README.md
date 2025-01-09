bd4d77d9760783253258dd3afb2e86f533bf9060
Arne Van Looveren <arne-vl@hotmail.be>
2025-01-09 17:41:08 +0100

    sequential execution of workflow

--- README.md
+++ README.md
@@ -2,22 +2,16 @@
 Flowrs is a high-performance Python package built in Rust, designed to simplify task orchestration and workflow automation. It combines Rust’s speed and reliability with Python’s accessibility, making it a powerful yet user-friendly solution for managing complex processes.
 
 ## Usage
-### Defining a Task
-```python
-from flowrs import Task
-
-def my_task():
-	print("Task executed!")
-
-task = Task("example_task", my_task)
-```
-
 ### Defining a Workflow
 ```python
 from flowrs import Workflow
 
+def example_task:
+	# implement task
+	return
+
 workflow = Workflow("example_workflow")
-workflow.add_task(task)
+workflow.add_task("example_task", task)
 
 workflow.run()
 ```
--- tests/test_flowrs.py
+++ tests/test_flowrs.py
@@ -1,12 +1,33 @@
 import unittest
 from flowrs import Workflow
+from typing import List
 
 class TestFlowrs(unittest.TestCase):
     def test_workflow_initialization(self):
         """Test Workflow initialization with name and function."""
 
         workflow = Workflow("dummy_workflow")
+
         self.assertEqual(workflow.get_name(), "dummy_workflow")
+
+    def test_workflow_sequential(self):
+        """Test Workflow sequential execution."""
+        number_list: List[int] = []
+
+        def task_one():
+            number_list.append(1)
+
+        def task_two():
+            number_list.append(2)
+
+        workflow = Workflow("dummy_workflow")
+
+        workflow.add_task("Task One", task_one)
+        workflow.add_task("Task Two", task_two)
+
+        workflow.run()
+
+        self.assertEqual(number_list, [1, 2])
 
 if __name__ == "__main__":
     unittest.main()
